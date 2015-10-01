76147746e6c9eb5bd00df23ca7603b0c88af3bc3
Juan Alberto Reséndiz Arteaga <jresendiz27@gmail.com>
2015-09-30 19:05:18 -0500

    Optimizing game of life engine, first approach

--- GameOfLife/src/main/groovy/mx/ipn/escom/complexSystems/gameOfLife/engine/GameOfLife.groovy
+++ GameOfLife/src/main/groovy/mx/ipn/escom/complexSystems/gameOfLife/engine/GameOfLife.groovy
@@ -1,22 +1,27 @@
 package mx.ipn.escom.complexSystems.gameOfLife.engine
+
+import groovyx.gpars.GParsPool
+
 /**
  * Created by alberto on 21/09/15.
  */
 @Singleton
 class GameOfLife {
-    short[][] neighborhood
-    short generation = 0
-    short rows
-    short columns
+    short[][] neighborhood;
+    short generation = 0;
+    short rows;
+    short columns;
     short alive = 0;
-    List newAlive
-    List newDeath
-    boolean start = false
-    Generator generator = new Generator()
+    short S_MIN_VALUE = 2;
+    short S_MAX_VALUE = 3;
+    List newAlive;
+    List newDeath;
+    boolean start = false;
+    Generator generator = new Generator();
 
     def init(int rows, int columns) {
-        this.rows = rows
-        this.columns = columns
+        this.rows = rows;
+        this.columns = columns;
         this.neighborhood = neighborhood ?: generator.generateRandomArray(this.rows, this.columns)
     }
 
@@ -28,40 +33,40 @@ class GameOfLife {
                     continue;
                 }
                 if (this.neighborhood[row % this.rows][column % this.columns] == 1) {
-                    numberOfNeighbors += 1
+                    numberOfNeighbors += 1;
                 }
             }
         }
         return numberOfNeighbors
     }
 
-    Map gameOfLife() {
+    public void gameOfLife() {
         this.generation += 1
         def newAlive = []
         def newDeath = []
         short alive = 0;
-        short[][] clonedArray = new short[rows][columns]
+        short[][] clonedArray = new short[rows][columns];
         for (short row = 0; row < rows; row++) {
             for (short column = 0; column < columns; column++) {
                 short cellNeighbors = this.getNumberOfNeighbors(row, column)
-                if (cellNeighbors < 2 || cellNeighbors > 3) {
+                if (cellNeighbors < S_MIN_VALUE || cellNeighbors > S_MAX_VALUE) {
                     // Dies of loneliness or Overpopulation
                     if (this.neighborhood[row][column] == 1) {
-                        newDeath.add([row, column])
+                        newDeath.add([row, column]);
                     }
                     clonedArray[row][column] = 0;
                     continue;
                 }
-                if (cellNeighbors == 3 && this.neighborhood[row][column] == 0) {
+                if (cellNeighbors == S_MAX_VALUE && this.neighborhood[row][column] == 0) {
                     // It was a dead cell, a new one bears
-                    newAlive.add([row, column])
-                    alive += 1
-                    clonedArray[row][column] = 1
+                    newAlive.add([row, column]);
+                    alive += 1;
+                    clonedArray[row][column] = 1;
                     continue;
                 }
-                if ((cellNeighbors == 3 || cellNeighbors == 2) && this.neighborhood[row][column] == 1) {
+                if ((cellNeighbors == S_MAX_VALUE || cellNeighbors == S_MIN_VALUE) && this.neighborhood[row][column] == 1) {
                     // Survives
-                    alive += 1
+                    alive += 1;
                     clonedArray[row][column] = 1;
                     continue;
                 } else { // it was a dead cell, nothing else happens
@@ -71,11 +76,18 @@ class GameOfLife {
 
             }
         }
-        this.neighborhood = clonedArray
-        this.newAlive = newAlive
-        this.newDeath = newDeath
-        this.alive = alive
-        return [newAlive: newAlive, newDeath: newDeath, currentPopulation: this.neighborhood, alive: alive, generation: this.generation]
+        this.neighborhood = clonedArray;
+        this.newAlive = newAlive;
+        this.newDeath = newDeath;
+        this.alive = alive;
+        return
+    }
+
+    boolean concurrentGameOfLife() {
+        GParsPool.withPool(2) { ->
+            // TODO implement concurrent gameOfLife method
+        };
+        return true
     }
 
     def resizeNeighborhood(int rows, int columns) {
