c31fcac07131a3b53536e5ba4d653403cb4a275f
Juan Alberto Reséndiz Arteaga <jresendiz27@gmail.com>
2015-10-19 00:08:19 -0500

    Updating opts.sh file, using opengl flags

--- Practices/opts.sh
+++ Practices/opts.sh
@@ -1,1 +1,2 @@
 export JAVA_OPTS='-server -Xms1024m -Xmx1024m -XX:PermSize=512m -XX:MaxPermSize=512m -XX:+UseParallelOldGC -XX:+UseAdaptiveSizePolicy -XX:+UseBiasedLocking'
+export _JAVA_OPTIONS='-Dsun.java2d.opengl=true -Dsun.java2d.xrender=true'
