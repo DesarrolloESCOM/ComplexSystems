1fa17854abf73f16819be85ceeeade63e02be964
Juan Alberto Reséndiz Arteaga <jresendiz27@gmail.com>
2015-10-18 23:53:57 -0500

    Exploring 4x4 universe generated by automata

---
+++ Practices/src/main/groovy/mx/ipn/escom/complexsystems/scripts/generator/ExploringDiffusionRule_4x4.groovy
@@ -0,0 +1,52 @@
+package mx.ipn.escom.complexsystems.scripts.generator
+
+import com.gmongo.GMongo
+import groovyx.gpars.GParsPool
+
+//
+
+def mongo = new GMongo()
+def db = mongo.getDB("GameOfLifeHistory");
+
+def size = 5
+
+def folder = "/home/alberto/Desktop/ComplexSystems/GameOfLife/"
+def currentIndex = "${size}x${size}/"
+
+new File(folder.concat(currentIndex)).mkdirs();
+println("Done making dirs")
+
+//
+def generateDotFiles(allStates) {
+    def mongo = new GMongo()
+    def db = mongo.getDB("GameOfLifeHistory");
+
+    def size = 5
+
+
+    def folder = "/home/alberto/Desktop/ComplexSystems/GameOfLife/"
+    def currentIndex = "${size}x${size}/"
+    def file
+
+    for (state in allStates) {
+        def golState = db["states_$size"].findOne([decimalState: state.decimalState]);
+        def currentStates = [state.decimalState]
+        while (currentStates.count(golState.decimalState) <= 1) {
+            golState = db["states_$size"].findOne([decimalState: golState.contains]);
+            currentStates.add(golState.decimalState);
+        }
+        if (currentStates.count(0) == 0) {
+            file = new File(folder.concat(currentIndex) + "GoL_${state.decimalState}.dot")
+            file.write("diagraph{ ${currentStates.join(" -> ")} }")
+        }
+
+
+    }
+}
+GParsPool.withPool {
+    def total = db["states_$size"].find([contains: [$gt: 0]], [decimalState: 1, _id: 0]).count()
+    def allStates = db["states_$size"].find([contains: [$gt: 0]], [decimalState: 1, _id: 0])
+    generateDotFiles(allStates)
+}
+
+println("Finishing generating dot files")
--- Practices/src/main/groovy/mx/ipn/escom/complexsystems/scripts/generator/ExploringGameOfLife_4x4.groovy
+++ Practices/src/main/groovy/mx/ipn/escom/complexsystems/scripts/generator/ExploringGameOfLife_4x4.groovy
@@ -3,33 +3,29 @@ package mx.ipn.escom.complexsystems.scripts.generator
 import com.gmongo.GMongo
 
 def mongo = new GMongo()
-def db = mongo.getDB("GameOfLifeHistory");
+def db = mongo.getDB("DiffusionRuleHistory");
 
 def size = 4
 def allStates = db["states_$size"].find([contains: [$gt: 0]], [decimalState: 1, _id: 0])
 
-def statesMap = [:]
+def folder = "/home/alberto/Desktop/ComplexSystems/DiffusionRule/"
+def currentIndex = "${size}x${size}/"
+def file
+
+new File(folder.concat(currentIndex)).mkdirs();
+println("Done making dirs")
 for (state in allStates) {
     def golState = db["states_$size"].findOne([decimalState: state.decimalState]);
-    statesMap["${state.decimalState}"] = [state.decimalState]
-    while (statesMap["${state.decimalState}"].count(golState.decimalState) <= 1) {
+    def currentStates = [state.decimalState]
+    while (currentStates.count(golState.decimalState) <= 1) {
         golState = db["states_$size"].findOne([decimalState: golState.contains]);
-        statesMap["${state.decimalState}"].add(golState.decimalState);
+        currentStates.add(golState.decimalState);
     }
-}
-println("Finished getting all the states")
-def folder = "/home/alberto/Desktop/ComplexSystems/GameOfLife/"
-def currentIndex = "${size}x${size}/"
-def collected = statesMap.findAll { st ->
-    return st.value.count(0) == 0
-}
-println("Finished filtering all the states")
-def file
-//
-new File(folder.concat(currentIndex)).mkdirs();
-//
-for (state in collected) {
-    file = new File(folder.concat(currentIndex) + "GoL_${state.key}.dot")
-    file.write("diagraph{ ${state.value.join(" -> ")} }")
+    if (currentStates.count(0) == 0) {
+        file = new File(folder.concat(currentIndex) + "GoL_${state.decimalState}.dot")
+        file.write("diagraph{ ${currentStates.join(" -> ")} }")
+    }
+
+
 }
 println("Finishing generating dot files")
