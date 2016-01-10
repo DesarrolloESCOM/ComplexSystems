bdf6a0fadaa1d8420faf1f8d0ab266209f1b0b86
Juan Alberto Reséndiz Arteaga <jresendiz27@gmail.com>
2016-01-10 01:00:14 -0600

    Creating elements files, including scenario and animals

---
+++ Practices/src/main/groovy/mx/ipn/escom/complexsystems/microworld/definition/elements/animals/Carnivore.groovy
@@ -0,0 +1,38 @@
+package mx.ipn.escom.complexsystems.microworld.definition.elements.animals
+
+import mx.ipn.escom.complexsystems.microworld.definition.impl.WorldElement
+
+/**
+ * Created by alberto on 9/01/16.
+ */
+class Carnivore implements WorldElement {
+    @Override
+    void drink() {
+
+    }
+
+    @Override
+    void eat() {
+
+    }
+
+    @Override
+    Map locationInformation() {
+        return null
+    }
+
+    @Override
+    void move() {
+
+    }
+
+    @Override
+    void reproduce() {
+
+    }
+
+    @Override
+    void generatePlants() {
+
+    }
+}
---
+++ Practices/src/main/groovy/mx/ipn/escom/complexsystems/microworld/definition/elements/scenario/NatureElement.groovy
@@ -0,0 +1,39 @@
+package mx.ipn.escom.complexsystems.microworld.definition.elements.scenario
+
+import mx.ipn.escom.complexsystems.microworld.definition.impl.WorldElement
+
+/**
+ * Created by alberto on 9/01/16.
+ */
+class NatureElement implements WorldElement {
+
+    @Override
+    void drink() {
+
+    }
+
+    @Override
+    void eat() {
+
+    }
+
+    @Override
+    Map locationInformation() {
+        return null
+    }
+
+    @Override
+    void move() {
+
+    }
+
+    @Override
+    void reproduce() {
+
+    }
+
+    @Override
+    void generatePlants() {
+
+    }
+}
