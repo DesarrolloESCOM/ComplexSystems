acf67d6c6a59262b4b8768c4ea25f9de45a003c9
Juan Alberto Reséndiz Arteaga <jresendiz27@gmail.com>
2016-01-09 18:03:52 -0600

    Updating opt.sh, adding /usr/bin/env bash

--- Practices/opts.sh
+++ Practices/opts.sh
@@ -1,2 +1,3 @@
-export JAVA_OPTS='-server -Xms1024m -Xmx1024m -XX:PermSize=512m -XX:MaxPermSize=512m -XX:+UseParallelOldGC -XX:+UseAdaptiveSizePolicy -XX:+UseBiasedLocking'
+#!/usr/bin/env bash
+export JAVA_OPTS='-server -Xms2048m -Xmx2048m -XX:PermSize=1024m -XX:MaxPermSize=1024m -XX:+UseParallelOldGC -XX:+UseAdaptiveSizePolicy -XX:+UseBiasedLocking'
 export _JAVA_OPTIONS='-Dsun.java2d.opengl=true -Dsun.java2d.xrender=true'
--- Practices/src/main/groovy/mx/ipn/escom/complexsystems/explore/ExploreAutomata.groovy
+++
@@ -1,7 +0,0 @@
-package mx.ipn.escom.complexsystems.explore
-
-/**
- * Created by alberto on 17/10/15.
- */
-class ExploreAutomata {
-}
