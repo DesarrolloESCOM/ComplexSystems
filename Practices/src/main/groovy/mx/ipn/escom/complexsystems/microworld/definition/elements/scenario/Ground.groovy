7747e70e703b86701cf214a71a7c75dca3c13527
Juan Alberto Reséndiz Arteaga <jresendiz27@gmail.com>
2016-01-10 18:33:47 -0600

    Generating lifeless elements, moving them into a new package

---
+++ Practices/src/main/groovy/mx/ipn/escom/complexsystems/microworld/definition/elements/scenario/Ground.groovy
@@ -0,0 +1,9 @@
+package mx.ipn.escom.complexsystems.microworld.definition.elements.scenario
+
+import mx.ipn.escom.complexsystems.microworld.definition.impl.WorldElement
+
+/**
+ * Created by alberto on 10/01/16.
+ */
+class Ground implements WorldElement {
+}
