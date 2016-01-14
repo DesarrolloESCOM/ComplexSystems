09b23ceb33ae28a1d7692d022cf5a911419f9506
Juan Alberto Reséndiz Arteaga <jresendiz27@gmail.com>
2016-01-14 02:28:42 -0600

    Modifyin Reproduction process on hebivores, it cost less

--- Practices/src/main/groovy/mx/ipn/escom/complexsystems/microworld/definition/elements/Herbivore.groovy
+++ Practices/src/main/groovy/mx/ipn/escom/complexsystems/microworld/definition/elements/Herbivore.groovy
@@ -36,4 +36,31 @@ class Herbivore implements WorldElement {
             }
         }
     }
+
+    void reproduce() {
+        int rows = worldCopy.length
+        int columns = worldCopy[0].length
+        int partialRow;
+        int partialColumn;
+        boolean hasBorn = false;
+        if (life > 25) {
+            for (int row = position[0] - 1; (row <= position[0] + 1) && !hasBorn; row++) {
+                for (int column = position[1] - 1; (column <= position[1] + 1) && !hasBorn; column++) {
+                    if (position[1] == column && position[0] == row) {
+                        continue;
+                    }
+                    partialRow = (row % rows) < 0 ? (row % rows) + rows : (row % rows);
+                    partialColumn = (column % columns) < 0 ? (column % columns) + columns : (column % columns);
+                    if (worldCopy[partialRow][partialColumn].type == WorldTypes.Ground.value) {
+                        WorldElement element = Class.forName(canonicalName).newInstance()
+                        element.position = [partialRow, partialColumn]
+                        element.worldCopy = this.worldCopy
+                        worldCopy[partialRow][partialColumn] = element
+                        hasBorn = true;
+                        decreaseLife(15);
+                    }
+                }
+            }
+        }
+    }
 }
